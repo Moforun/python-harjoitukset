8e4494b68fce5b24cf524bca7bbf9a0e57d0d537
Moforun <vigur98@gmail.com>
2026-09-02 16:22:43 +0300

    Delete tehtavat/Muuttujat ja vuorovaikutteiset ohjelmat3.py

--- tehtavat/Muuttujat ja vuorovaikutteiset ohjelmat3.py
+++
@@ -1,7 +0,0 @@
-kanta = float(input("Anna suorakulmion kanta: "))
-korkeus = float(input("Anna suorakulmion korkeus: "))
-piiri = 2 * (kanta + korkeus)
-pintaala = kanta * korkeus
-
-print("suorakulmion piiri: ", piiri) 
-print("suorakulmion pinta-ala: ", pintaala)
